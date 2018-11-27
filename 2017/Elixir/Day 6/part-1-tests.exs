a1e12da7e3f32e70173a83c52c08c5c6c2f60ebd
Maxim Gritchin <max.gritchin@gmail.com>
2018-11-27 21:34:48 +0300

    add day 6 part 1

--- 2017/Elixir/Day 6/part-1-tests.exs
+++ 2017/Elixir/Day 6/part-1-tests.exs
@@ -2,8 +2,8 @@ defmodule Part1Tests do
   use ExUnit.Case
   Code.require_file("part-1.exs")
 
-  @tag :skip
+  #@tag :skip
   test "0, 2, 7, and 0 takes 5 cycles" do
-    assert Banks.count_of_cycles("0 2 7 0", " ") == 5
+    assert Banks.redistribute("0 2 7 0", " ") == 5
   end
 end
--- 2017/Elixir/Day 6/part-1.exs
+++ 2017/Elixir/Day 6/part-1.exs
@@ -1,4 +1,4 @@
-_="""
+_ = """
 
 The debugger would like to know how many redistributions can be done before a blocks-in-banks configuration is produced that has been seen before.
 
@@ -22,104 +22,101 @@ At this point, we've reached a state we've seen before: 2 4 1 2 was already seen
 
 Given the initial block counts in your puzzle input, how many redistribution cycles must be completed before a configuration is produced that has been seen before?
 """
+
 defmodule Banks do
-  def count_of_cycles(banks, splitter) when is_bitstring(banks) do
+  def redistribute(banks, delim) when is_bitstring(banks) do
     banks
-    #|> IO.inspect(label: "Input ")
-    |> String.split(splitter)
-    # convert to map for O(1) access (like an array)
-    |> Enum.with_index(1) |> Enum.map(fn {v, i} -> {i, String.to_integer(v)} end) |> Map.new()
-    #|> IO.inspect(label: "Map ")
-    |> calculation(MapSet.new())
-    |> IO.inspect(label: "Count of cycles ")
+    |> String.split(delim)
+    |> redistribute()
   end
-
-  defp calculation(banks, seen) do
-    #IO.inspect binding(), [label: "Calculation method input params "]
-    cond do
-      seen |> MapSet.member?(banks) -> MapSet.size(seen) # exit if already has been seen
-      true ->
-        # find the biggest bank
-        {current_index, value} = banks
-        |> get_biggest_bank()
-        #|> IO.inspect(label: "Biggest bank value ")
-        # redistribute across the banks
-        {inc_value, value_to_left} = calc_cycle(Map.size(banks), value)
-        #|> IO.inspect(label: "Values for cycles ")
-        # update
-        new_banks = cond do
-          inc_value > 0 ->
-            banks
-            |> Map.put(current_index, cond do
-                value_to_left > 0 -> 1
-                true -> 0
-            end) # update bank value that was redistributed
-            |> update_all([except: current_index, inc_by: inc_value])
-            |> update([from: current_index, inc_by: 1, count: value_to_left - 1])
-          true -> # not all list update
-            banks
-            |> Map.put(current_index, 0) # update bank value that was redistributed
-            |> update([from: current_index, inc_by: 1, count: value])
-        end
-        #|> IO.inspect(label: "New bank(updated) ")
-        # recurtion
-        calculation(new_banks, MapSet.put(seen, banks))
-    end
-  end
-
-  defp get_biggest_bank(banks) do
+  def redistribute(banks) when is_list(banks) do
     banks
-    |> Enum.reduce({0, 0}, fn
-      {_, v}, {c_i, c_v} when c_v >= v -> {c_i, c_v}
-      {i, v}, _ -> {i, v}
-    end)
-  end
-  defp calc_cycle(banks_count, value_to_reduce) do
-    count_of_full_cycles = div(value_to_reduce, banks_count - 1)
-    count_of_left = value_to_reduce - (count_of_full_cycles * (banks_count - 1))
-
-    {count_of_full_cycles, count_of_left}
+    |> convert_to_array()
+    |> process()
+    |> IO.inspect(label: "Count of redistribution processes ")
   end
 
-  defp update_all(banks, opts) do
-    # get input values
-    except_index = opts |> Keyword.get(:except)
-    inc_by = opts |> Keyword.get(:inc_by)
+  defp convert_to_array(banks) do
+    banks
+    |> Enum.map(fn x -> String.to_integer(x) end)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {v, i} -> {i, v} end)
+    |> Map.new()
+    |> IO.inspect(label: "Array ")
+  end
+
+  defp find_max_bank(state) do
+    # find bank with the biggest amount of blocks
+    state |> Enum.max_by(fn {_idx, val} -> val end)
+  end
+
+  defp process(state), do: process(state, MapSet.new(), false)
+  defp process(_state, seen, true), do: MapSet.size(seen)
+  defp process(state, seen, _done) do
+    # add as seen
+    seen = seen |> MapSet.put(state)
+    # find bank to redistribute
+    bank_to_redistribute = state |> find_max_bank()
+    #|> IO.inspect(label: "Bank to redistribute ")
     # update
-    for {index, value} <- banks, into: %{} do
-      cond do
-        except_index == index -> {index, value}
-        true -> {index, value + inc_by}
-      end
-    end
-  end
-  defp update(banks, opts) do
-    #IO.inspect binding()
-    cond do
-      opts |> Keyword.get(:count) <= 0 -> banks
-      true ->
-        # get input values
-        current_index = opts |> Keyword.get(:from)
-        inc_by = opts |> Keyword.get(:inc_by)
-        count = opts |> Keyword.get(:count)
-        # update
-        {index, value} = get_next(banks, current_index)
-        update(banks |> Map.put(index, value + inc_by), [from: index, inc_by: 1, count: count - 1])
-    end
+    new_state = state
+    |> make(bank_to_redistribute)
+    #|> IO.inspect(label: "New state ")
+    # recursion
+    #IO.gets("continue...")
+    process(new_state, seen, MapSet.member?(seen, new_state))
   end
 
-  defp get_next(banks, index) do
-    cond do
-      Map.size(banks) <= index ->
-        {1, banks |> Map.get(1)} # at begin
+  defp make(state, {index, blocks}) do
+    # calc add value number
+    add_val = div(blocks, Map.size(state) - 1)
+    blocks_left = blocks - (add_val * (Map.size(state) - 1))
+    # update
+    state
+    |> update(add_val, index, (Map.size(state) - 1), blocks_left)
+  end
+
+  defp update(state, _add, from, 0, blocks_left) when blocks_left in [0, 1] do
+    state
+    |> set_elem(from, blocks_left)
+  end
+  defp update(state, _add, from, 0, blocks_left) when blocks_left > 1 do # need to redistribute a little bit more
+    state
+    |> update(1, from, blocks_left - 1, 1)
+  end
+  defp update(state, 0, from, _count, block_left) do
+    state
+    |> update(1, from, block_left, 0)
+  end
+  defp update(state, add, from, count, blocks_left) do
+    # element to update
+    {idx, val} = state |> get_elem(from + count)
+    # update
+    new_state = state |> set_elem(idx, val + add)
+    # recurtion
+    update(new_state, add, from, count - 1, blocks_left)
+  end
+
+  defp get_elem(state, index) do
+    cond  do
+      Map.size(state) >= index ->
+        {index, state |> Map.get(index)}
       true ->
-        {index + 1, banks |> Map.get(index + 1)}
+        idx = index - Map.size(state)
+        {idx, state |> Map.get(idx)}
+    end
+  end
+  defp set_elem(state, index, value) do
+    cond  do
+      Map.size(state) >= index ->
+        state |> Map.put(index, value)
+      true ->
+        state |> Map.put(index - Map.size(state), value)
     end
   end
 end
 
-
 "4	10	4	1	8	4	9	14	5	1	14	15	0	15	3	5"
-|> Banks.count_of_cycles("\t")
+|> Banks.redistribute("\t")
 
 # 12841
