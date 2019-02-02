3f87374eca70a5cbe8be831dd03fe17c19995fb2
Maxim Gritchin <max.gritchin@gmail.com>
2019-02-02 22:19:26 +0300

    update Day Part 1 Day 10

--- 2017/Elixir/Day 10/part-1.exs
+++ 2017/Elixir/Day 10/part-1.exs
@@ -5,8 +5,6 @@ lengths = "187,254,0,81,169,219,1,190,19,102,255,56,46,32,2,216"
 |> Enum.map(&String.to_integer(&1))
 
 get_first_and_second = fn x->
-  #[first | [ second | _]] = Map.keys(x)
-  #{Map.get(x, first), Map.get(x, second)}
   {Map.get(x, 1), Map.get(x, 2)}
 end
 
