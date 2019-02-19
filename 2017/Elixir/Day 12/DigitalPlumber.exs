3b729bd08c327822b3bc80096c7779e91d7938b5
Maxim Gritchin <max.gritchin@gmail.com>
2019-02-19 21:49:03 +0300

    Add Day 12 part 2

--- 2017/Elixir/Day 12/DigitalPlumber.exs
+++ 2017/Elixir/Day 12/DigitalPlumber.exs
@@ -16,6 +16,28 @@ defmodule DigitalPlumber do
 
     find_all_related(input, [elements | rest] |> List.flatten(), new_set)
   end
+
+  @doc """
+  Calculate count of groups
+  """
+  def count_of_groups(input) when is_map(input) do
+    # find first group
+    set = {1, input |> find_all_related([0], MapSet.new([0]))}
+
+    {groups_count, _} = input |> Enum.reduce(set, fn {key, val}, {acc, seen} ->
+      cond do
+        MapSet.member?(seen, key) ->
+          {acc, seen}
+        true ->
+          new_group = input |> find_all_related([key], MapSet.new([key]))
+          {acc + 1, MapSet.union(seen, new_group)}
+      end
+    end)
+
+    groups_count
+  end
+
+
 end
 
 # TESTS
@@ -38,5 +60,21 @@ defmodule DigitalPlumberTests do
 
     # act, assert
     assert DigitalPlumber.count_of_programs(input, 0) == 6
+  end
+
+  test "calculates groups" do
+    # arrange
+    input = %{
+      0 => [2],
+      1 => [1],
+      2 => [0,3,4],
+      3 => [2,4],
+      4 => [2,3,6],
+      5 => [6],
+      6 => [4,5]
+    }
+
+    # act, assert
+    assert DigitalPlumber.count_of_groups(input) == 2
   end
 end
---
+++ 2017/Elixir/Day 12/part-2.exs
@@ -0,0 +1,19 @@
+_="""
+  https://adventofcode.com/2017/day/12
+"""
+
+Code.require_file(Path.join(__DIR__, "DigitalPlumber.exs"))
+
+# RUN
+File.read!(Path.join(__DIR__, "input.dat"))
+|> String.split("\n")
+|> Enum.map(fn x ->
+  [key, values] = String.split(x, "<->")
+  key = String.trim(key) |> String.to_integer
+  values = String.split(values, ",") |> Enum.map(&String.trim/1) |> Enum.map(&String.to_integer/1)
+
+  {key, values}
+end)
+|> Map.new
+|> DigitalPlumber.count_of_groups
+|> IO.inspect(label: "Result")
